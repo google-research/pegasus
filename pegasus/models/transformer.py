5dd46a54a23412b8fd5adfe2add1d109d1cd4ad1
Jacob Parnell <jacob.parnell@rozettatechnology.com>
2020-07-16 17:32:34 +1000

    To understand the loss function inputs and outputs

--- pegasus/models/transformer.py
+++ pegasus/models/transformer.py
@@ -129,7 +129,7 @@ class TransformerEncoderDecoderModel(base.BaseModel):
         weights=targets_mask_BxT)
 
     # Prints the loss
-    print("Loss: ".format(loss))
+    print("Loss: {}".format(loss))
 
     return loss, {"logits": logits_BxTxV}
 
